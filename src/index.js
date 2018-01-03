2a018814b4aa3d198d98a946c12d0fef825d4767
Rustam Khudaibergenov <ruslyandiya@mail.ru>
2018-01-03 11:58:06 +0300

    sEcond step

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+console.log('');
 const userName = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${userName} !`);
+console.log(`Hello, ${userName}!`);
 export default userName;
