f72850cb5bb09f9e035bb4017d2d2c66c0033e35
Rustam Khudaibergenov <ruslyandiya@mail.ru>
2018-01-07 10:05:12 +0300

    seven step

---
+++ src/games/balance.js
@@ -0,0 +1,38 @@
+import generatorNum from '../generatorNum';
+import game from '..';
+
+export default () => {
+  const gameParam = () => {
+    const description = 'Balance the given number.';
+    const givenNum = () => generatorNum(100, 10000);
+    const notBalanceNum = givenNum();
+    const question = `${notBalanceNum}`;
+
+    const balanceNum = (num) => {
+      const arrNum = String(num).split('').map(el => Number(el));
+      const lengthNum = arrNum.length;
+      const sumNum = arrNum.reduce((acc, el) => acc + parseInt(el, 10), 0);
+      const numOfIndexes = Math.trunc(sumNum / lengthNum);
+      const newNum = arrNum.fill(numOfIndexes);
+      const restPoints = Math.floor(sumNum % lengthNum);
+      if (restPoints === 0) {
+        return newNum.join('');
+      }
+      const func = (partNum, restP) => {
+        if (restP === 0) {
+          return partNum;
+        }
+        const newElement = partNum[0] + 1;
+        const fullNum = partNum.concat(newElement);
+        return func(fullNum, restP - 1);
+      };
+      newNum.splice(-restPoints, restPoints);
+      const result = func(newNum, restPoints);
+      return result.join('');
+    };
+
+    const correctAnswer = balanceNum(notBalanceNum);
+    return { description, question, correctAnswer };
+  };
+  return game(gameParam);
+};
