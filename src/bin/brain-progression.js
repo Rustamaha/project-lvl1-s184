1dae58ad9b0533656f39e6e88317bfd46ab407da
Rustam Khudaibergenov <ruslyandiya@mail.ru>
2018-01-07 11:44:01 +0300

    step eight

---
+++ src/bin/brain-progression.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import progression from '../games/progression';
+
+
+progression();
---
+++ src/games/progression.js
@@ -0,0 +1,24 @@
+import generatorNum from '../generatorNum';
+import game from '..';
+
+export default () => {
+  const gameParam = () => {
+    const description = 'What number is missing in this progression?';
+    const arrOfNum = generatorNum(2, 9);
+    const firstNum = generatorNum(10, 90);
+    const progressNum = [];
+    const progression = (progressNumbers, num, progressNumber, indexes) => {
+      if (indexes === 0) {
+        return progressNumbers;
+      }
+      const progress = num + progressNumber;
+      const arrOfProgress = progressNumbers.concat(progress);
+      return progression(arrOfProgress, progress, progressNumber, indexes - 1);
+    };
+    const ourProgression = progression(progressNum, firstNum, arrOfNum, 10);
+    const correctAnswer = ourProgression.splice(arrOfNum, 1, '..');
+    const question = ourProgression.join(' ');
+    return { description, question, correctAnswer };
+  };
+  return game(gameParam);
+};
